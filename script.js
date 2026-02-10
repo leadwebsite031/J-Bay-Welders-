45f9ce2d5c798b844af8f49082e3fc9ff2ddacba
leadwebsite031 <leadwebsite031@gmail.com>
2026-02-11 01:22:12 +0200

    Update and rename qualifications.html to script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// Mobile Navigation Toggle
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
+}
+
+// Smooth Scroll for local links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+navSlide();
